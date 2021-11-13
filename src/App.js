1e7e427d43d905384679f30abdbe9189a285c1a6
nkleinmann <nekleinmann@gmail.com>
2021-11-13 13:20:05 -0500

    Add starter files

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import bitters from '../src/_assets/bitters.jpg';
+import './App.css';
+import Blurb from '../src/components/blurb'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={bitters} className='App-logo' alt="drink with bitters" />
+        {/* https://unsplash.com/photos/YeH5EIRFCIs */}
+        <p>
+          Bitter Boys
+        </p>
+        <a
+          className="App-link"
+          href="https://shaquanda-will-feed-you.myshopify.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          App link
+        </a>
+      <Blurb />
+      </header>
+    </div>
+  );
+}
+
+export default App;
