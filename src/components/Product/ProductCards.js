9b42c87a33e41125c50742ac991fb3b401a826da
nkleinmann <nekleinmann@gmail.com>
2021-11-20 15:09:45 -0500

    update filler data for cards

--- src/components/Product/ProductCards.js
+++ src/components/Product/ProductCards.js
@@ -7,13 +7,13 @@ function Product(props) {
             <Card key={props.productInfo.title} style={{ width: '18rem' }} className="m-4">
             <Card.Img 
             variant="top" 
-            src="https://images.unsplash.com/photo-1631262553859-945f7fb49bba?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Yml0dGVyc3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+            src={props.productInfo.image}
             alt="Cocktail" />
             <Card.Body>
                 <Card.Title>{props.productInfo.title}</Card.Title>
-                <Card.Subtitle className="mb-2 text-muted">{props.productInfo.subtitle}</Card.Subtitle>
-                <Card.Text>{props.productInfo.text}</Card.Text>
-                <Card.Link href={props.productInfo.link}>Check it out</Card.Link>
+                <Card.Subtitle className="mb-2 text-muted">{props.productInfo.price}</Card.Subtitle>
+                {/* <Card.Text>{props.productInfo.text}</Card.Text>
+                <Card.Link href={props.productInfo.link}>Check it out</Card.Link> */}
             </Card.Body>
             </Card>
         </>
