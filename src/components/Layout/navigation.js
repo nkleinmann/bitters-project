7b80cf1bb2b96ca0c57167c027945360cdb75a7a
ambertrand <alex.m.bertrand@gmail.com>
2021-11-20 11:44:41 -0500

    adding navbar links and styling

--- src/components/Layout/navigation.js
+++ src/components/Layout/navigation.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 import bitters from '../../_assets/bitters.jpg';
 
 function Navigation() {
   return (
     <>
-      <Navbar bg='light'>
+      <Navbar className='nav' bg='light'>
         <Navbar.Brand href='#home'>
           <img
             src={bitters}
@@ -15,7 +16,13 @@ function Navigation() {
             alt='React Bootstrap logo'
           />
         </Navbar.Brand>
-        <p>Boy Bitters</p>
+        <p className='brand'>Boy Bitters</p>
+        <Nav.Link href='#home' className='navlink home'>
+          Home
+        </Nav.Link>
+        <Nav.Link href='#about' className='navlink about'>
+          About
+        </Nav.Link>
       </Navbar>
     </>
   );
