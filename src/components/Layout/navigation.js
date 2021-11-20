beae0d331bfdf2308529e7b03522f8f12d63e2ce
ambertrand <alex.m.bertrand@gmail.com>
2021-11-20 12:52:53 -0500

    update image and file name

--- src/components/Layout/navigation.js
+++ src/components/Layout/navigation.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import bitters from '../../_assets/bitters.jpg';
+import boybitters from '../../_assets/boybitters.jpg';
 
 function Navigation() {
   return (
     <Navbar className='nav' bg='light'>
       <Navbar.Brand href='#home'>
         <img
-          src={bitters}
-          width='30'
-          height='30'
+          src={boybitters}
+          width='50'
+          height='50'
           className='d-inline-block align-top logo'
           alt='React Bootstrap logo'
         />
