5f4e13bee7e4a6730c463d630d94076dd26e34ba
ambertrand <alex.m.bertrand@gmail.com>
2021-11-20 12:38:48 -0500

    updating navigation

--- src/components/Layout/navigation.js
+++ src/components/Layout/navigation.js
@@ -5,26 +5,28 @@ import bitters from '../../_assets/bitters.jpg';
 
 function Navigation() {
   return (
-    <>
-      <Navbar className='nav' bg='light'>
-        <Navbar.Brand href='#home'>
-          <img
-            src={bitters}
-            width='30'
-            height='30'
-            className='d-inline-block align-top logo'
-            alt='React Bootstrap logo'
-          />
-        </Navbar.Brand>
-        <p className='brand'>Boy Bitters</p>
-        <Nav.Link href='#home' className='navlink home'>
-          Home
-        </Nav.Link>
-        <Nav.Link href='#about' className='navlink about'>
-          About
-        </Nav.Link>
-      </Navbar>
-    </>
+    <Navbar className='nav' bg='light'>
+      <Navbar.Brand href='#home'>
+        <img
+          src={bitters}
+          width='30'
+          height='30'
+          className='d-inline-block align-top logo'
+          alt='React Bootstrap logo'
+        />
+      </Navbar.Brand>
+      <span className='brand'>Boy Bitters</span>
+      <Nav.Link href='/' style={{ color: 'black' }} className='navlink home'>
+        Home
+      </Nav.Link>
+      <Nav.Link
+        href='#about'
+        style={{ color: 'black' }}
+        className='navlink about'
+      >
+        About
+      </Nav.Link>
+    </Navbar>
   );
 }
 
