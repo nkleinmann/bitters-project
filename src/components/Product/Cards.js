4f4c4075e13258758b89075b71d43e9cbd2141ed
nkleinmann <nekleinmann@gmail.com>
2021-11-20 13:11:14 -0500

    adding cardInfo.js

--- src/components/Product/Cards.js
+++ src/components/Product/Cards.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card'
+import bitters from './cardInfo'
 
 function Cards() {
+    console.log(bitters)
     return(
         <div className="text-align">
             <Card style={{ width: '18rem' }}>
@@ -10,7 +12,7 @@ function Cards() {
                 src="https://images.unsplash.com/photo-1631262553859-945f7fb49bba?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Yml0dGVyc3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
                 alt="Cocktail" />
                 <Card.Body>
-                    <Card.Title>Hi</Card.Title>
+                    <Card.Title>{bitters[0].title}</Card.Title>
                     <Card.Subtitle className="mb-2 text-muted">Card</Card.Subtitle>
                     <Card.Text>Text</Card.Text>
                     <Card.Link href="#">Link</Card.Link>
---
+++ src/components/Product/cardInfo.js
@@ -0,0 +1,23 @@
+const bitters = [
+    {
+        title: 'orange bitters',
+        subtitle: 'flavor',
+        test: 'description',
+        link: 'https://www.google.com'
+    },
+    {
+        title: 'apple bitters',
+        subtitle: 'flavor',
+        test: 'description',
+        link: 'https://www.google.com'
+    },
+    {
+        title: 'grapefruit bitters',
+        subtitle: 'flavor',
+        test: 'description',
+        link: 'https://www.google.com'
+    },
+]
+
+export default bitters 
+
